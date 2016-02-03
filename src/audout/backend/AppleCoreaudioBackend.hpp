996150527f76138bd7e7abffe6ef388d28832fd8
Ivan Gagis <igagis@gmail.com>
2016-02-03 22:46:12 +0200

    stuff

--- src/audout/backend/AppleCoreaudioBackend.hpp
+++ src/audout/backend/AppleCoreaudioBackend.hpp
@@ -45,6 +45,64 @@ class AppleCoreaudioBackend{
 		}
 	} audioComponent;
 	
+	static OSStatus outputCallback(
+			void *inRefCon,
+			AudioUnitRenderActionFlags * ioActionFlags,
+			const AudioTimeStamp * inTimeStamp,
+			UInt32 inBusNumber, UInt32 inNumberFrames,
+			AudioBufferList * ioData
+		)
+	{
+		/*
+		SDL_AudioDevice *this = (SDL_AudioDevice *) inRefCon;
+		AudioBuffer *abuf;
+		UInt32 remaining, len;
+		void *ptr;
+		UInt32 i;
+
+		//Only do anything if audio is enabled and not paused
+		if (!this->enabled || this->paused) {
+			for (i = 0; i < ioData->mNumberBuffers; i++) {
+				abuf = &ioData->mBuffers[i];
+				SDL_memset(abuf->mData, this->spec.silence, abuf->mDataByteSize);
+			}
+			return 0;
+		}
+
+		// No SDL conversion should be needed here, ever, since we accept
+		// any input format in OpenAudio, and leave the conversion to CoreAudio.
+		
+		//   SDL_assert(!this->convert.needed);
+		//   SDL_assert(this->spec.channels == ioData->mNumberChannels);
+
+		for (i = 0; i < ioData->mNumberBuffers; i++) {
+			abuf = &ioData->mBuffers[i];
+			remaining = abuf->mDataByteSize;
+			ptr = abuf->mData;
+			while (remaining > 0) {
+				if (this->hidden->bufferOffset >= this->hidden->bufferSize) {
+					// Generate the data
+					SDL_LockMutex(this->mixer_lock);
+					(*this->spec.callback)(this->spec.userdata,
+								this->hidden->buffer, this->hidden->bufferSize);
+					SDL_UnlockMutex(this->mixer_lock);
+					this->hidden->bufferOffset = 0;
+				}
+
+				len = this->hidden->bufferSize - this->hidden->bufferOffset;
+				if (len > remaining)
+					len = remaining;
+				SDL_memcpy(ptr, (char *)this->hidden->buffer +
+						   this->hidden->bufferOffset, len);
+				ptr = (char *)ptr + len;
+				remaining -= len;
+				this->hidden->bufferOffset += len;
+			}
+		}
+*/
+		return 0;
+	}
+	
 public:
 	AppleCoreaudioBackend(
 			audout::AudioFormat outputFormat,
@@ -52,11 +110,64 @@ public:
 			audout::Listener* listener
 		)
 	{
-		//TODO:
+		if(AudioUnitInitialize(this->audioComponent.instance)){
+			throw audout::Exc("Failed to initialize audio unit instance");
+		}
+		
+		AudioStreamBasicDescription formatDesc;
+		formatDesc.mSampleRate = outputFormat.frequency();
+		formatDesc.mFormatID = kAudioFormatLinearPCM;
+		formatDesc.mFormatFlags = kAudioFormatFlagIsSignedInteger;
+		formatDesc.mFramesPerPacket = 1;
+		formatDesc.mChannelsPerFrame = outputFormat.numChannels();
+		formatDesc.mBitsPerChannel = 16;
+		formatDesc.mBytesPerFrame = formatDesc.mChannelsPerFrame * 2;
+		formatDesc.mBytesPerPacket = formatDesc.mBytesPerFrame;
+		
+		if(AudioUnitSetProperty(
+				this->audioComponent.instance,
+				kAudioUnitProperty_StreamFormat,
+				kAudioUnitScope_Input,
+				0,
+				&formatDesc,
+				sizeof(formatDesc)
+			))
+		{
+			throw audout::Exc("Failed to set audio unit input property");
+		}
+		
+		AURenderCallbackStruct callback;
+		memset(&callback, 0, sizeof(callback));
+		callback.inputProc = &outputCallback;
+		callback.inputProcRefCon = listener;
+		
+		if(AudioUnitSetProperty(
+				this->audioComponent.instance,
+				kAudioUnitProperty_SetRenderCallback,
+				kAudioUnitScope_Input,
+				0,
+				&callback,
+				sizeof(callback)
+			))
+		{
+			throw audout::Exc("Unable to attach an IOProc to the selected audio unit");
+		}
+		
+		this->setPaused(false);
+	}
+	
+	~AppleCoreaudioBackend()noexcept{
+		this->setPaused(true);
 	}
 	
 	void setPaused(bool paused){
-		//TODO:
+		if(paused){
+			AudioOutputUnitStop(this->audioComponent.instance);
+		}else{
+			if(AudioOutputUnitStart(this->audioComponent.instance)){
+				throw audout::Exc("Unable to start audio unit");
+			}
+		}
 	}
 };
 
