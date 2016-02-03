931e7bc18dbe17c2c46472ad051aadea78a2e2ba
Ivan Gagis <igagis@gmail.com>
2016-02-03 22:05:03 +0200

    stuff

--- src/audout/backend/AppleCoreaudioBackend.hpp
+++ src/audout/backend/AppleCoreaudioBackend.hpp
@@ -7,12 +7,44 @@
 
 #pragma once
 
+#include "../Exc.hpp"
 #include "../AudioFormat.hpp"
 #include "../Listener.hpp"
+
+//#include <CoreAudio/CoreAudio.h>
+#include <AudioUnit/AudioUnit.h>
 
 namespace audout{
 
 class AppleCoreaudioBackend{
+	
+	struct AudioComponent{
+		AudioComponentInstance instance;
+		
+		AudioComponent(){
+			//open the default audio device
+			AudioComponentDescription desc;
+			desc.componentType = kAudioUnitType_Output;
+			desc.componentSubType = kAudioUnitSubType_DefaultOutput;
+			desc.componentFlags = 0;
+			desc.componentFlagsMask = 0;
+			desc.componentManufacturer = kAudioUnitManufacturer_Apple;
+
+			auto component = AudioComponentFindNext(nullptr, &desc);
+			if(!component){
+				throw audout::Exc("Failed to find default audio device");
+			}
+
+			if(AudioComponentInstanceNew(component, &this->instance)){
+				throw audout::Exc("Failed to open default audio device");
+			}
+		}
+		
+		~AudioComponent()noexcept{
+			AudioComponentInstanceDispose(this->instance);
+		}
+	} audioComponent;
+	
 public:
 	AppleCoreaudioBackend(
 			audout::AudioFormat outputFormat,
