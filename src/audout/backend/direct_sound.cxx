c4181e9eba7110a1d2d7d4ca6be8cd1e02ed4da4
Ivan Gagis <igagis@gmail.com>
2021-11-08 17:07:01 +0200

    fix windows build

--- src/audout/backend/direct_sound.cxx
+++ src/audout/backend/direct_sound.cxx
@@ -24,6 +24,32 @@ SOFTWARE.
 
 /* ================ LICENSE END ================ */
 
+/*
+The MIT License (MIT)
+
+Copyright (c) 2016-2021 Ivan Gagis <igagis@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+
+/* ================ LICENSE END ================ */
+
 #pragma once
 
 #include <utki/config.hpp>
@@ -137,7 +163,7 @@ class audio_backend : public utki::destructable{
 				throw std::runtime_error("SetCooperativeLevel() failed");
 			}
 
-			ds_scope_exit.reset();
+			ds_scope_exit.release();
 		}
 		~direct_sound()noexcept{
 			this->ds->Release();
@@ -222,7 +248,7 @@ class audio_backend : public utki::destructable{
 			
 			this->dsb->SetCurrentPosition(0);
 
-			dsb_scope_exit.reset();
+			dsb_scope_exit.release();
 		}
 		
 		~direct_sound_buffer()noexcept{
